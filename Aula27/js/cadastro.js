7e5b520025e810084d99883bbf0dbf483ee0e1e0
guilherguimaraes-cmd <guilhermeguimaraes@codificaedu.com>
2026-07-02 09:41:46 -0300

    atualização dos scripts

--- Aula27/js/cadastro.js
+++ Aula27/js/cadastro.js
@@ -1,24 +1,22 @@
-import { registrar } from './auth.js';
+import { registrar } from './autenticador.js'
 
-const form     = document.querySelector('#form-cadastro');
-const aviso    = document.querySelector('#aviso');
+const form = document.querySelector('#form-cadastro')
+const aviso = document.querySelector('#aviso')
 
-
-// 2) Registrar o usuário ao enviar o formulário
+//Registrar o usuário ao enviar o formulário
 form.addEventListener('submit', (evento) => {
-  evento.preventDefault();   // impede o recarregamento padrão da página
+    evento.preventDefault() //impede o recarregamento da pagina
+    
+    const usuario = {
+        email: document.querySelector('#email').value,
+        senha: document.querySelector('#senha').value
+    }
 
-  const usuario = {
-    email:      document.querySelector('#email').value,
-    senha:      document.querySelector('#senha').value,
-  };
-
-  try {
-    registrar(usuario);
-    alert('Cadastro realizado! Faça login para continuar.');
-    window.location.href = 'login.html';   // leva para a tela de login
-  } catch (erro) {
-    aviso.textContent = erro.message;
-  }
-});
-
+    try{
+        registrar(usuario)
+        alert('Cadastro realizado! Faça login para continuar.')
+        window.location.href = 'login.html'
+    }catch(erro){
+        aviso.textContent = erro.message
+    }
+})
